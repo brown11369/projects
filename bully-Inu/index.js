95514892b5e3f78507852726139460dd856231d7
Brown <brown11369@gmail.com>
2022-03-05 13:55:34 +0530

    nproject

---
+++ bully-Inu/index.js
@@ -0,0 +1,17 @@
+function copyToClipboard(execCopy){
+    let val = document.getElementById(execCopy).innerHTML;
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    alert('you have copied the Contract Address of Bully Inu')
+
+  }  
+
