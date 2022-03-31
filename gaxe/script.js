e6e7191dc2c6073c24dc4f16a004e6225fb65526
Brown <brown11369@gmail.com>
2022-03-31 11:09:35 +0530

    up

--- gaxe/script.js
+++ gaxe/script.js
@@ -58,11 +58,13 @@ function displaymoviestable(moviearr) {
     edit.classList.add("fa-solid");
     edit.classList.add("fa-pen-to-square");
     action.append(edit);
+    edit.onclick=updatemovie.bind(this,movie.id)
 
     let trash = document.createElement("i");
     trash.classList.add("fa-solid");
     trash.classList.add("fa-trash-can");
     action.append(trash);
+    trash.onclick=deltmovie.bind(this,movie.id);
 
     row.appendChild(action);
 
@@ -135,6 +137,117 @@ function addmovie(){
   document.getElementById("addDate").type="text";
   console.log(movie);
 
+
+}
+
+// update movie
+
+let movieupdate=null;
+
+
+function updatemovie(id){
+
+  movieupdateid=id;
+  let movie=movies.find((movie,index)=>{
+    return movie.id===id;
+  })
+
+  movieupdate=movie;
+
+  document.getElementById("updateTitle").value=movie.title;
+
+  let genres="";
+  movie.genres.forEach((genre,i)=>{
+    genres+=genre+",";
+  })
+
+  document.getElementById("updateGenre").value=genres.substring(0,genres.length-1);
+
+
+  document.getElementById("updateDuration").value=movie.duration;
+
+  document.getElementById("updateDate").value=movie.releaseDate;
+
+  let actors="";
+  movie.actors.forEach((actor,i)=>{
+    actors+=actor+",";
+  })
+  document.getElementById("updateActor").value=actors.substring(0,actors.length-1);
+
+
+  document.getElementById("updateImdb").value=movie.imdbRating;
+
+  document.getElementById("updateUrl").value=movie.posterurl;
+
+  document.getElementById("updatestoryLine").value=movie.storyline;
+
+
+
+  document.getElementById("updateModal").style.display="flex";
+}
+
+
+// edit upadte filed
+
+function updtmovie(){
+
+
+
+  movieupdate.title=document.getElementById("updateTitle").value;
+  movieupdate.genres=document.getElementById("updateGenre").value.split(",");
+  movieupdate.duration=document.getElementById("updateDuration").value;
+  movieupdate.releaseDate=document.getElementById("updateDate").value;
+  movieupdate.actors=document.getElementById("updateActor").value.split(",");
+  movieupdate.imdbRating=document.getElementById("updateImdb").value;
+  movieupdate.posterurl=document.getElementById("updateUrl").value;
+  movieupdate.storyline=document.getElementById("updatestoryLine").value;
+
+
+  localStorage.setItem("movies",JSON.stringify(movies));
+
+  displaymoviestable(movies);
+
+  document.getElementById("addDate").type="text";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //////////////////////////////////////////////////
+
+
+
+
+
+
+function deltmovie(id){
+
+  let cnfrm=confirm("are you sure you want to delete this movie")
+
+  if(cnfrm===true){
+    let index=movies.findIndex((movie,index)=>{
+      return movie.id===id;
+    })
+    
+    movies.splice(index,1)
+    localStorage.setItem("movies",JSON.stringify(movies));
+  
+    displaymoviestable(movies);
+  }
 
 }
 
